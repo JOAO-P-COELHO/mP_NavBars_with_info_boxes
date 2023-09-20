c4286dd08c69e50987a735e06f83b02aa8434d3b
JOAO-P-COELHO <143346573+JOAO-P-COELHO@users.noreply.github.com>
2023-09-20 22:57:36 +0100

    new

--- js_nav.js
+++ js_nav.js
@@ -1,14 +1,17 @@
+/* Menu - nav */
 $("#menu_img").click(function () { 
   $("#navigation").toggle(1000);
 });
+/* Menu - nav */
 
- $(".info-content").hide(); 
-
+/* First div */
+ $(".info-content").hide();
  $(".info-title").click(function () { 
   $(".info-content").slideToggle(1000);
-});
+}); 
+/* First div */
 
- 
+ /* Second div */
 $( ".content-box" ).click(function(){
   $( ".content-box" ).animate({
     width: "100%",
@@ -16,9 +19,15 @@ $( ".content-box" ).click(function(){
       }, 2000 );
   $(".content-after").show().animate({opacity: "1"},3000);
 });
+/* Second div */
 
+/* Third div - This part above makes my password hide or show */
 
-/* This part above makes my password hide or show */
+$(".form").hide();
+ $(".form-title").click(function () { 
+  $(".form").slideToggle(1000);
+}); 
+
 let viewLoginPwd = false;
 
 function changePwdView ()
@@ -35,6 +44,47 @@ else if (viewLoginPwd === true) {
   viewLoginPwd = false;
   }
 }
+/* Third div */
+
+/* Fourth div - It sends an alert with the src link of your image in background */
+
+$("#img-btn").click(function(){
+  var image = $("#imagex").attr("src");
+  alert(image);      
+});
+
+$(".img-box").hide();
+ $(".img-title").click(function () { 
+  $(".img-box").slideToggle(1000);
+}); 
+
+/* Fourth div */
+
+/* Fifth div */
+$(".hover-p").hide();
+$('.hover-title').mouseover(function() {
+  $('.hover-p').stop(true, true).show(1000);
+}).mouseout(function() {
+  $('.hover-p').stop(true, true).hide(1000);
+});
+/* Fifth div */
+
+
+
+/* Sixth div - toggle + showing coordinates*/
+
+$(".coord-p").hide();
+$(".coord-title").click(function(){
+  $(".coord-p").toggle(1000);
+});
+
+$(document).on('mousemove', function (e){
+  $(".coords").html("X: "+e.pageX +', Y:'+ e.pageY);
+});
+/* Sixth div */
+
+
+
 
 
 
